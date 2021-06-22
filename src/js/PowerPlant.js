0533380a0a6a6c73f9ca40ff3a6e351482716379
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-22 11:44:55 -0700

    WIP: Finish walkthrough. Break for lunch.
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>

--- src/js/PowerPlant.js
+++ src/js/PowerPlant.js
@@ -38,21 +38,27 @@ const changeState = (prop) => {
 }
 
 const feed = changeState("soil")
-feed(5)(plant)
+const hydrate = changeState("water");
+const giveLight = changeState("light");
+
+const blueFood = changeState("soil")(5)
+const greenFood = changeState("soil")(10)
+const yuckyFood = changeState("soil")(-5)
+
+const goodWater = changeState("water")(5)
+
 
 const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction) => {
+  let currentState = { soil: 0, light: 0, water: 0 };
+  return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = { ...newState };
     return newState;
   }
 }
 
-const blueFood = changeState("soil")(5)
-const greenFood = changeState("soil")(10)
-const yuckyFood = changeState("soil")(-5)
-
 const stateControl = storeState()
-
 const fedPlant = stateControl(blueFood)
+const plantFedAgain = stateControl(greenFood)
+const wateredPlant = stateControl(goodWater)
+const siccPlant = stateControl(yuckyFood)
